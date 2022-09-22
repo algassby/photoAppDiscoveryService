42e219b2d7fd6689fc8826595ec3a8c319a204c6
algassby <algassby@hotmail.com>
2022-09-22 15:14:35 +0200

    project config

---
+++ src/test/java/com/barry/cours/app/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barry.cours.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
